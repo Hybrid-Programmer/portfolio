78655f6dc9bc0326419a6b78c5df2b3d578f58c7
Hybrid_programmer <95778417+Hybrid-Programmer@users.noreply.github.com>
2022-10-31 22:24:57 +0100

    Add files via upload

---
+++ js/router.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+	
+	$('#crypto').on('click', function () {
+		window.location = 'https://www.cryptoinvestway.com/';
+	});
+
+	$('#sucf').on('click', function () {
+		window.location = ' https://hybrid-programmer.github.io/sucf-unn/';
+	});
+
+	$('#acn').on('click', function () {
+		window.location = ' https://hybrid-programmer.github.io/landing-page/';
+	});
+
+	$('#community').on('click', function () {
+		window.location = ' https://hybrid-programmer.github.io/unntv/';
+	});
+
+})
